8f10c35cab313cee8df6c0b5065d36304f38d912
BadASszZ <tobbewestlin@hotmail.com>
2021-03-13 12:14:45 +0100

    Updated comments
    Updated comments

--- src/IotWebConf.h
+++ src/IotWebConf.h
@@ -1,9 +1,11 @@
 /**
- * IotWebConf.h -- IotWebConf is an ESP8266/ESP32
+ * IoTJediWebConfSettings.h -- IoTJediWebConf is an ESP8266/ESP32
  *   non blocking WiFi/AP web configuration library for Arduino.
- *   https://github.com/prampec/IotWebConf
- *
+ *   https://github.com/prampec/IoTJediWebConf
+ * --- Based on https://github.com/prampec/IotWebConf ---
  * Copyright (C) 2020 Balazs Kelemen <prampec+arduino@gmail.com>
+ * 
+ * Changed by IoT_Jedi to work together with Visuino
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
